d23ec11329d25a8cf759e51170e2c6a5ece31cb7
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-08-04 00:16:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 mega-webdav ./ --public
-rclone --config "rclone.conf" copy Blaze: FMega:Test_Backup --transfers 300  --stats-one-line -P  --log-file “rlog.txt” --log-level INFO  --stats 2s
+rclone --config "rclone.conf" copy DropBox: FMega:Test_Backup --transfers 200  --stats-one-line -P  --log-file “rlog.txt” --log-level INFO  --stats 2s
